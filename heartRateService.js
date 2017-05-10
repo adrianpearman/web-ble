10ab3522d4824ab37b4ce52086ad964c077efded
Adrian Pearman <adrianpearman12@gmail.com>
2017-05-09 20:25:43 -0400

    completed follow along code session

--- heartRateService.js
+++ heartRateService.js
@@ -5,31 +5,68 @@ export default class HeartRateService {
 		this.characteristics = new Map()
 	}
 
-	// connect() {
+	connect() {
+		navigator.bluetooth.requestDevice({
+			filters:[{services: ['heart_rate']}]
+		})
+		.then(device => {
+			this.device = device
+			return device.gatt.connect()
+		})
+		.then(server => {
+			this.server = server
+			return server.getPrimaryService('heart_rate')
+		})
+		.then(service => {
+			return Promise.all([
+			this.cacheCharacteristic(service, 'body_sensor_location'),
+			this.cacheCharacteristic(service, 'heart_rate_measurement')
+			])
+		})
+	}
 
-	// }
+	disconnect() {
+		this.device.gatt.disconnect()
+	}
 
-	// disconnect() {
+	cacheCharacteristic(service, characteristicUuid) {
+		return service.getCharacteristic(characteristicUuid)
+		.then(characteristic => {
+			this.characteristics.set(characteristicUuid, characteristic)
+		})
+	}
 
-	// }
+	readSensorLocation() {
+		const char = this.characteristics.get('body_sensor_location')
+		return char.readValue()
+		.then(value=> {
+			let sensorLocation = value.getUint8(0);
+			switch(sensorLocation){
+				case 0: return 'Other';
+				case 1: return 'Chest';
+				case 2: return 'Wrist';
+				case 3: return 'Finger';
+				case 4: return 'Hand';
+				case 5: return 'Ear Lobe';
+				case 6: return 'Foot';
+				default: return 'Unknown';
+			}
+		})
+	}
 
-	// cacheCharacteristic(service, characteristicUuid) {
+	startHeartRateNotifications() {
+		const char = this.characteristics.get('heart_rate_measurement')
+		return char.stopHeartRateNotifications()
+		.then(() => char)
+	}
 
-	// }
+	stopHeartRateNotifications() {
+		const char = this.characteristics.get('heart_rate_measurement')
+		return char.stopNotifications()
+			.then(() => char)
+	}
 
-	// readSensorLocation() {
-
-	// }
-
-	// startHeartRateNotifications() {
-
-	// }
-
-	// stopHeartRateNotifications() {
-
-	// }
-
-	// parseHeartRate(value) {
-
-	// }
+	parseHeartRate(value) {
+		return value.getUint8(1)
+	}
 }
